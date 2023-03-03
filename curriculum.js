a5f887b643263c8c243ecfc3363712b8c504b469
mip86 <123782000+mip86@users.noreply.github.com>
2023-03-03 11:40:11 -0300

    Add files via upload

---
+++ curriculum.js
@@ -0,0 +1,8 @@
+document.getElementById("boton").onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("texto").innerHTML = "Deportista, Gran lector"}
+
+    
+document.getElementById("colores").onclick = function () {
+    console.log("capturamos el evento click");
+    document.body.style.backgroundColor= "gray"; }    
